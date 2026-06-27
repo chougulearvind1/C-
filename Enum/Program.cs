dfeaab5d58c93a5eb5c8f56c3ba1699d2e69e990
Arvind chougule <chougulearvind1@gmail.com>
2026-06-27 09:49:00 +0530

    enum

---
+++ Enum/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Enum1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int daynuber=
+(int)DaysOfWeek.Monday;
+       Console.WriteLine(daynuber);
+
+       int [] daynumbers = (int[])Enum.GetValues(typeof(DaysOfWeek));
+       foreach (int day in daynumbers)
+       {
+        Console.WriteLine(day);
+       }
+       int monthnumber = (int)months.November;
+       Console.WriteLine(monthnumber);
+    }
+}
+public enum DaysOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+public enum months
+{
+    January=1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November=45,
+    December
+}
